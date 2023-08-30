bac63e66abf54a4c4b9c0fc66e8d10c6dc926422
Roman <jrj80mail@gmail.com>
2023-08-31 00:44:46 +0300

    next edit

--- src/js/template/markup.js
+++ src/js/template/markup.js
@@ -8,8 +8,19 @@ const markupContainer = document.getElementById('container-id');
 let markup = '';
 
 books.forEach(category => {
-  markup += `<li class="home-books-container fade-up">
+  markup += `<li class="home-books-container">
                    <h2 class="books-best">${category[0].list_name}</h2>
                    <ul class="book-by-category-list">`;
+
+  if (category.length) {
+    markup += category
+      .map(({ _id, author, book_image, title }) => {
+        return `<li class="home-book-card" book-id="${_id}">
+                    </li>`;
+      })
+      .join('');
+  } else {
+    markup += '<p class="no-books">No books in this category.</p>';
+  }
   markup += '</ul></li>';
 });
