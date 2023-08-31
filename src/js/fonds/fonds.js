4393cb32d673f5a5c40fa63e7b9bebda07d28c77
hotgorod <hotgorod@gmail.com>
2023-08-31 14:27:51 +0100

    reset.scss added to common.scss

--- src/js/fonds/fonds.js
+++ src/js/fonds/fonds.js
@@ -1,5 +1,13 @@
 import { fonds } from '../api/api-fonds';
 
 const li = ({ title, url, img }) => {
-    
+      return `<a href="${url}"
+      ><li class="fonds__item">
+        <span class="number__list">01</span>
+        <img
+          class="img__fonds"
+          src="/src/images/fonds/save-the-children.png"
+          alt="${title}"
+        /></li
+    ></a>`
 }
