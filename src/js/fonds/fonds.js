97e6c8ecc7be3d4cc70ab29efd2b0a2f4ac3674f
hotgorod <hotgorod@gmail.com>
2023-08-31 15:48:14 +0100

    created img for fonds-object

--- src/js/fonds/fonds.js
+++ src/js/fonds/fonds.js
@@ -1,13 +1,26 @@
 import { fonds } from '../api/api-fonds';
 
+// export function createMarkupFonds(fonds) {
+//     return fonds.map(({title, url, img})=>`<a href="${url}"
+//       ><li class="fonds__item">
+//         <span class="number__list">01</span>
+//         <img
+//           class="img__fonds"
+//           src="${img}"
+//           alt="${title}"
+//         /></li
+//     ></a>`).join('');
+// }
 const li = ({ title, url, img }) => {
       return `<a href="${url}"
       ><li class="fonds__item">
         <span class="number__list">01</span>
         <img
           class="img__fonds"
-          src="/src/images/fonds/save-the-children.png"
+          src="${img}"
           alt="${title}"
         /></li
     ></a>`
 }
+const liElements = fonds.map(li).join('');
+//  
