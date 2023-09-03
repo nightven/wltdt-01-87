bcb6c7b261d3b6a4585b006f802ce18e65c5471b
ServerIbraimovUa <serveribraimov7@gmail.com>
2023-09-03 17:25:58 +0300

    add header auth

--- src/js/refs/refs.js
+++ src/js/refs/refs.js
@@ -1,19 +1,20 @@
 export default {
   modal: document.querySelector('.backdrop'),
-  fromEl: document.querySelector('.js-form'),
+  signUpForm: document.querySelector('.js-form'),
   signInEl: document.querySelector('.js-sign-in'),
   btnSignIn: document.querySelector('.js-sign-in'),
   listCategoryEl: document.querySelector('.js-category'),
   listAllBooksEl: document.querySelector('.js-books'),
   categoryNameEl: document.querySelector('.category'),
-  spanColorEl : document.querySelector('.color-title'),
-  spanNormalEl : document.querySelector('.normal-title'),
+  spanColorEl: document.querySelector('.color-title'),
+  spanNormalEl: document.querySelector('.normal-title'),
 
   //authorizetion
   authorizedDiv: document.getElementById('authorized'),
   unauthorizedDiv: document.getElementById('unauthorized'),
   logoutButton: document.getElementById('logout'),
   loginButton: document.getElementById('login'),
+  nameUserEl: document.querySelector('.js-login-user'),
 
   //book modal
   listBookModalEl: document.querySelector('.book-list'),
