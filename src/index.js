5ced9dfde8002e40ab92c15a2902fb82e103f3fe
Dmytro Yankovskiy <dimayanik97@gmail.com>
2023-09-04 12:13:19 +0200

    js switcher
    only js logic

--- src/index.js
+++ src/index.js
@@ -105,3 +105,20 @@ async function onShowMoreBooks(event) {
     console.log(error.message);
   }
 }
+
+//-----------------------------Theme Switcher-------------------------------------------------------
+
+const switcherEl = document.querySelector('.switch__input');
+
+switcherEl.addEventListener('change', onColorSwitch);
+
+function onColorSwitch() {
+  const currentTheme = document.documentElement.getAttribute('data-theme');
+  if(currentTheme === 'light') {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+  }
+}
+
+//---------------------------------------------------------------------------------------------------
