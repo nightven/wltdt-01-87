65f29008d590796dfe70b64517b970b61ee8f6df
Vitaliy Beyar <37002507+nightven@users.noreply.github.com>
2023-09-04 17:38:41 +0200

    add file theme-switcher

--- src/index.js
+++ src/index.js
@@ -128,19 +128,4 @@ navLinks.forEach(link => {
   });
 });
 
-//-----------------------------Theme Switcher-------------------------------------------------------
 
-const switcherEl = document.querySelector('.switch__input');
-
-switcherEl.addEventListener('change', onColorSwitch);
-
-function onColorSwitch() {
-  const currentTheme = document.documentElement.getAttribute('data-theme');
-  if (currentTheme === 'light') {
-    document.documentElement.setAttribute('data-theme', 'dark');
-  } else {
-    document.documentElement.setAttribute('data-theme', 'light');
-  }
-}
-
-//---------------------------------------------------------------------------------------------------
