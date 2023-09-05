f2d8b90d150fae09f3fc3f29f3e9e099b13fafb0
Dmytro Yankovskiy <dimayanik97@gmail.com>
2023-09-05 16:21:13 +0200

    switcher
    switcher local update

--- src/index.js
+++ src/index.js
@@ -25,7 +25,9 @@ import refs from './js/refs/refs';
 import { data } from 'jquery';
 import axios from 'axios';
 import { ref } from 'firebase/database';
-import { onColorSwitch } from './js/themes/theme-switcher';
+import { onColorSwitch, switcherCheck } from './js/themes/theme-switcher';
+
+
 
 // showLoader
 function showLoader() {
@@ -37,6 +39,9 @@ function showLoader() {
 function hideLoader() {
   refs.loader.style.display = 'none';
 }
+
+// switcherStatus
+switcherCheck();
 
 //!submit form register
 // refs.seeButtonEl.addEventListener('click', e => {
@@ -187,3 +192,4 @@ navLinks.forEach(link => {
     link.classList.add('current');
   });
 });
+
