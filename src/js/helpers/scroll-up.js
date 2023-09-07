1beceb15f9366869c36c41b59182ff80a92d62f2
ServerIbraimovUa <serveribraimov7@gmail.com>
2023-09-07 14:31:55 +0300

    fix

--- src/js/helpers/scroll-up.js
+++ src/js/helpers/scroll-up.js
@@ -1,14 +1,12 @@
 let scrollToTopBtnEl = document.querySelector('.scroll-up');
 
-window.onscroll = function () {
+window.onscroll = () => {
   scrollFunction();
 };
-
 function scrollFunction() {
   scrollToTopBtnEl.style.display =
-     document.documentElement.scrollTop > 300
-      ? 'flex'
-      : 'none';
+    document.documentElement.scrollTop > 350 ? 'flex' : 'none';
+  console.log(document.documentElement.scrollTop);
 }
 
 scrollToTopBtnEl.addEventListener('click', backToTop);
